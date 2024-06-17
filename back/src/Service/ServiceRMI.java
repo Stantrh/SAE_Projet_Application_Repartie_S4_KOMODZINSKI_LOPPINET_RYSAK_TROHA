2caf77beb085c587ee599c0db307f8bc2405b842
Jawad Komodzinski <jawad.komodzinski6@etu.univ-lorraine.fr>
2024-06-17 15:53:30 +0200

    insertion restaurant

--- back/src/Service/ServiceRMI.java
+++ back/src/Service/ServiceRMI.java
@@ -9,5 +9,5 @@ public interface ServiceRMI extends Remote {
 
     public String getAllRestaurant() throws RemoteException;
     public String reserverTable(String nom, String prenom, int nbPersonne, String tel, int idRestaurant) throws RemoteException;
-
+    public String ajouterRestaurant(String nom, String adresse, Double latitude, Double longitude) throws RemoteException;
 }
--- back/src/Service/ServiceRestaurant.java
+++ back/src/Service/ServiceRestaurant.java
@@ -109,4 +109,66 @@ public class ServiceRestaurant extends UnicastRemoteObject implements ServiceRMI
             throw new RemoteException("Erreur SQL", e);
         }
     }
+
+
+    @Override
+    public String ajouterRestaurant(String nom, String adresse, Double latitude, Double longitude) throws RemoteException {
+        try {
+            // Désactiver l'auto-commit pour gérer les transactions manuellement
+            connexion.setAutoCommit(false);
+
+            // Préparation de la requête d'insertion
+            String requete = "INSERT INTO restaurants (Nom, adresse, latitude, longitude) VALUES (?, ?, ?, ?)";
+            PreparedStatement preparedStatement = connexion.prepareStatement(requete);
+
+            // Remplissage des paramètres de la requête
+            preparedStatement.setString(1, nom);
+            preparedStatement.setString(2, adresse);
+            preparedStatement.setDouble(3, latitude);
+            preparedStatement.setDouble(4, longitude);
+
+            // Exécution de la requête
+            int changementEffectue = preparedStatement.executeUpdate();
+
+            // Valider la transaction si l'insertion a réussi
+            if (changementEffectue > 0) {
+                // Récupérer l'identifiant généré
+                ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    int restaurantId = generatedKeys.getInt(1);
+                    connexion.commit();
+                    JsonArrayBuilder jsonArrayBuilder = Json.createArrayBuilder();
+                    JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder().add("RestaurantID", restaurantId);
+                    jsonArrayBuilder.add(jsonObjectBuilder);
+                    JsonStructure jsonStructure = jsonArrayBuilder.build();
+                    StringWriter stringWriter = new StringWriter();
+                    // Écriture de la structure JSON dans un StringWriter
+                    try (JsonWriter jsonWriter = Json.createWriter(stringWriter)) {
+                        jsonWriter.write(jsonStructure);
+                    }
+                    // Retourne la chaîne JSON
+                    return stringWriter.toString();
+                } else{
+                    connexion.rollback();
+                    return "";
+                }
+            } else {
+                // Annuler la transaction si l'insertion a échoué
+                connexion.rollback();
+                return "";
+            }
+
+        } catch (SQLException e) {
+            try {
+                if (connexion != null) {
+                    // Annuler la transaction en cas de problème SQL
+                    connexion.rollback();
+                }
+            } catch (SQLException rollbackException) {
+                System.out.println("Erreur lors de l'annulation de la transaction");
+            }
+            System.out.println("Problème avec une requête SQL");
+            throw new RemoteException("Erreur SQL", e);
+        }
+    }
 }
