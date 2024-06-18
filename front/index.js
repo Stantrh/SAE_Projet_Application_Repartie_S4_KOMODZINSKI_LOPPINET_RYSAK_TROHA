5f3f8a9f4fe9f3b86e1242c4afceb0e8701d3cdd
Stantrh <stanislas.troha8@etu.univ-lorraine.fr>
2024-06-18 11:15:31 +0200

    petit probleme en moins

--- front/index.js
+++ front/index.js
@@ -6,8 +6,5 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 
     await initMap();
-
-    console.log("FETCH DE l'API DONNEES BLOQUEES");
-    const rep = await fetchApi('https://www.datagrandest.fr/data4citizen/d4c/api/datasets/1.0/1642070072496-1/alternative_exports/90c43af4-e5b9-4069-8bf1-61a5b900b476/')
-
+    
 });
